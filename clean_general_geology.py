01316902fa6bfc5cb5c826b4fb1a88294abbec28
Logan Weeks <60147557+lweeks20@users.noreply.github.com>
2020-02-13 15:27:54 -0800

    Update clean_general_geology.py

--- clean_general_geology.py
+++ clean_general_geology.py
@@ -16,8 +16,23 @@ df=df.rename(columns={'AREA':'index_location','TITLE':'title','DATE':'date','NOT
 
 #remove "NUM" column
 df = df.drop(["NUM"], axis=1)
+df1 = df1.drop(["longitude"], axis=1)
+df1 = df1.drop(["latitude"], axis=1)
 
 #reorder columns to put 'id' first
 cols = df.columns.tolist()
 cols.insert(0, cols.pop(cols.index('id')))
 df = df.reindex(columns= cols)
+
+coordinates= pd.read_excel(r'/Users/LoganWeeks/Desktop/NBMG work stuff/Converted Coordinates.xlsx')
+df2=pd.DataFrame(coordinates)
+
+df2 = df2.drop(["UTME NAD27"], axis=1)
+df2 = df2.drop(["UTMN NAD27"], axis=1)
+
+df = [df1, df2]
+
+done= (pd.concat(df, axis=1, ignore_index=False))
+
+#for some reason there's an error "AttributeError: 'list' object has no attribute 'to_excel'"
+df.to_excel(r'/Users/LoganWeeks/Desktop/NBMG work stuff/general_geology02112020.xlsx', index = None, header=True)
